0a7e86b32cd86b662e323b0319dc2faee038b475
Joshwa Tesoro <104410384+jshwatsoro@users.noreply.github.com>
2023-02-04 08:35:53 -0800

    Create database, api, and controllers.

---
+++ server/controllers/spanishController.js
@@ -0,0 +1,8 @@
+const db = require('../database/db.json');
+const spanishController = {};
+
+spanishController.getSpanishWords = async(req, res, next) => {
+    const query = 
+}
+
+module.exports = spanishController;
