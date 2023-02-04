24e9c0a8a348df4776ee081d6443f4f491d167c2
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-02-04 12:15:28 -0500

    create board skeleton, create timer

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,1 +1,37 @@
-// test test
+import React, { useState } from 'react';
+import InputBox from 'InputBox.jsx';
+import Timer from 'Timer.jsx';
+import Blanks from 'Blanks.jsx';
+
+// state passed from App via props:
+// isTimeout (starts at false, set to true if run out of time)
+// setRoundScore (starts at 0, updates on isGuessed)
+// score
+// roundNumber
+// englishWord
+// foreignWord
+
+function Board(props) {
+  const [guessed, isGuessed] = useState(false);
+  const [timeRemaining, updateTime] = useState(15);
+  if (guessed === true) {
+    props.setRoundScore(timeRemaining * 100);
+  }
+  if (timeRemaining <= 0) {
+    props.isTimeout(true);
+  }
+  return (
+    <div id='gameboard'>
+      <div id='scorebox'>
+        <div id='roundDisplay'>Round {props.roundNumber} of 10</div>
+        <div id='scoreDisplay'>Total Score: {props.score}</div>
+      </div>
+      <Timer timeRemaining={timeRemaining} updateTime={updateTime} />
+      <Blanks foreignWord={props.foreignWord} />
+      <div id='englishword'>{props.englishWord}</div>
+      <InputBox foreignWord={props.foreignWord} isGuessed={isGuessed} />
+    </div>
+  );
+}
+
+export default Board;
