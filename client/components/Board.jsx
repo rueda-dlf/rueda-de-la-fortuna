0fc9345a6dc3a587d36bee7446cd73749863fd12
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-02-04 14:09:46 -0500

    change variable names

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -4,7 +4,7 @@ import Timer from 'Timer.jsx';
 import Blanks from 'Blanks.jsx';
 
 // state passed from App via props:
-// isTimeout (starts at false, set to true if run out of time)
+// setTimeout (starts at false, set to true if run out of time)
 // setRoundScore (starts at 0, updates on isGuessed)
 // score
 // roundNumber
@@ -15,16 +15,19 @@ function Board(props) {
   const [guessed, isGuessed] = useState(false);
   const [timeRemaining, updateTime] = useState(15);
   if (guessed === true) {
-    props.setRoundScore(timeRemaining * 100);
+    props.setScore({
+      totalScore: props.score.totalScore + timeRemaining * 100,
+      roundScore: timeRemaining * 100,
+    });
   }
   if (timeRemaining <= 0) {
-    props.isTimeout(true);
+    props.setTimeout(true);
   }
   return (
     <div id='gameboard'>
       <div id='scorebox'>
         <div id='roundDisplay'>Round {props.roundNumber} of 10</div>
-        <div id='scoreDisplay'>Total Score: {props.score}</div>
+        <div id='scoreDisplay'>Total Score: {props.score.totalScore}</div>
       </div>
       <Timer timeRemaining={timeRemaining} updateTime={updateTime} />
       <Blanks foreignWord={props.foreignWord} />
