916203062ec2b0e354ebd5753c76a271a15f974c
Ashlee Gafaru <114522387+ashleegaf@users.noreply.github.com>
2023-02-06 11:57:06 -0500

    Revert "Add start screen"

--- server/controllers/spanishController.js
+++ server/controllers/spanishController.js
@@ -1,44 +1,53 @@
-//declare variables to get access to the module
+//const db = require('../model/Models');
+//const db = require('./database/db.json')
+
 const path = require('path');
-const fs = require('fs').promises;
-const spanishController = {};
-//have the finaldatabase in the global scope so that we can access it outside of the function
-let finalDatabase;
-//fetch the data in the local file
-async function fetchAndTransformData() {
+const fs = require('fs');
+
+fs.readFile(path.join(__dirname,'../database/test.json'), 'utf-8', (err, data) => {
+   if(err){
+      console.error(err);
+      return;
+   }
    try {
-      //parse the data 
-      const data = await fs.readFile(path.join(__dirname, '../database/test.json'), 'utf-8');
-      const db = JSON.parse(data);
-      //declare transform data so that we can return that data into a new and usable form of data structure 
+   const db = JSON.parse (data);
+console.log(db)
       const transformData = (db) => {
          return Object.entries(db).map(([spanishWord, englishWordObj]) => {
-            return {spanishWord, englishWord: englishWordObj.translations[0]};
+         return {spanishWord, englishWord: englishWordObj.translations[0]};
          });
       };
-      //reassing the new and final database 
-      finalDatabase = transformData(db);
-      console.log(finalDatabase)
-      await fs.writeFile(path.join(__dirname, '../database/transformed-data.json'), JSON.stringify(finalDatabase), 'utf-8');
-     //catch any errors
+   const finalDatabase = transformData(db)
+   console.log(finalDatabase)
+   return finalDatabase
+   
    } catch (err) {
-      console.log('Error Parsing JSON string!', err);
+      console.log('Error Parsing JSON string!', err)
    }
+})
+
+
+
+// { words: [{casa: {translation: [house]}}, {comida: {translation: [food]}}]}
+// word[i]translation[0]
+
+
+const spanishController = {};
+
+spanishController.getSpanishWords = async(req, res, next) => {
+   const { words } = req.body;
+console.log('what is this', words)
+
+   try{
+    const result = await db.query(words)
+    console.log(result)
+    res.locals.words = result.words;
+    return next()
+   }catch(e){
+    return next(e)
+   }
+   
 }
-
-
-
-//testing to see if the function is working 
-fetchAndTransformData();
-// console.log(typeof fetchAndTransformData)
-// //declare middleware function to try and gain access to the database and storing it into the reslocals.. 
-// spanishController.getSpanishWords = async (req, res, next) => {
-//    await fetchAndTransformData();
-//    res.locals.finalDatabase = finalDatabase;
-//    console.log(finalDatabase);
-//    next();
-// };
-
 
 module.exports = spanishController;
 
